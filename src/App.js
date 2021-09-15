c68ea687ae0f617557ddc88641b3dd779976bed1
WN00163949 <bassit.agbere@wuerth-it.com>
2021-09-15 15:24:07 +0200

    add fonts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ListMakerComponent } from './components/listMaker/listMakerComponent';
 function App() {
   return (
     <div className="main">
+      <h1 id="title">TODO LIST</h1>
       <WelcomeScreenComponent />
       <ListMakerComponent />
     </div>
