fa3bc22c51e6601b0d7bb75d3fd2a53628ca19e1
joenae <jona.woelfl@gmx.net>
2021-09-15 18:52:19 +0200

    Push CSS Checkbox

--- src/components/welcomeScreen/welcomeScreenComponent.js
+++ src/components/welcomeScreen/welcomeScreenComponent.js
@@ -15,6 +15,10 @@ export function WelcomeScreenComponent() {
     <div className="welcomeScreenComponent">
       <p id="welcomeFont">Herzlich Willkommen zur ToDo-Liste!</p>
       <button id="welcomeButton" onClick={hide}>Jetzt loslegen</button>
+      <div id="checkboxAll">
+      <input type="checkbox" id="welcomeCheckbox"></input>
+      <label for="welcomeCheckbox">Diesen Bildschirm nicht mehr anzeigen!</label>
+      </div> 
     </div>)
   }
   return <div className='backgroundForDiv'></div>
